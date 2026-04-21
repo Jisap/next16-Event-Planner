192ef02120fcdb6c27fb56834d5324470853fcc2
Jorge Matos <jisapjisap@gmail.com>
2026-04-21 15:58:04 +0200

    feat: integrate Neon Auth and Prisma for authentication and database management

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,10 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Link from "next/link";
+import { NeonAuthUIProvider, UserButton } from "@neondatabase/auth/react";
+import { authClient } from "@/lib/auth/client";
+
+
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,24 +31,32 @@ export default function RootLayout({
     <html
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      suppressHydrationWarning
     >
       <body className="min-h-full flex flex-col bg-background text-foreground">
-        <header className="sticky top-0 z-50 border-b border-border bg-surface/90 backdrop-blur">
-          <div className="container mx-auto px-4 h-16 max-w-6xl flex items-center justify-between">
-            <Link href="/" className="text-xl font-bold text-primary">
-              Event Planner
-            </Link>
-
-            <nav className="flex items-center gap-4">
-              <Link href="/dashboard" className="text-muted hover:text-foreground transition-colors">
-                Dashboard
+        <NeonAuthUIProvider
+          authClient={authClient as any}
+        >
+          <header className="sticky top-0 z-50 border-b border-border bg-surface/90 backdrop-blur">
+            <div className="container mx-auto px-4 h-16 max-w-6xl flex items-center justify-between">
+              <Link href="/" className="text-xl font-bold text-primary">
+                Event Planner
               </Link>
-            </nav>
-          </div>
-        </header>
-        <main className="mx-auto flex w-full max-w-6xl flex-col px-4 py-8">
+
+              <nav className="flex items-center gap-4">
+                <Link href="/dashboard" className="text-muted hover:text-foreground transition-colors">
+                  Dashboard
+                </Link>
+
+                <UserButton size="icon" />
+              </nav>
+            </div>
+          </header>
+
+          <main className="mx-auto flex w-full max-w-6xl flex-col px-4 py-8">
             {children}
-        </main>
+          </main>
+        </NeonAuthUIProvider>
       </body>
     </html>
   );
